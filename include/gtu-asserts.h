d3f79ca80a503f42981fe221656e99bfcbadf254
Bob131 <bob@bob131.so>
2018-01-25 07:54:29 +1100

    add gtk-doc documentation
    Adds an optional compile-time dependency on GTKDoc and documents all of
    the public GTU API in the include/ headers.

--- include/gtu-asserts.h
+++ include/gtu-asserts.h
@@ -1,31 +1,96 @@
 #ifndef __GII_TEST_UTILS_ASSERTS_H__
 #define __GII_TEST_UTILS_ASSERTS_H__
 
+/**
+ * SECTION:gtu-asserts
+ * @short_description: aborting tests early
+ * @title: Asserts and skips
+ * @include: gtu.h
+ *
+ * GTU provides flow control management to unit tests, automatically backing
+ * out of test execution when a test is aborted. To facilitate this mechanism,
+ * we provide some drop-in replacements for GLib's assertion functions that can
+ * be used within your unit tests.
+ *
+ * Please note that the result of calling any of these functions outside of a
+ * GTU test case is undefined.
+ */
+
 #ifndef __GII_TEST_UTILS_H__
 #error "Only <gtu.h> can be included directly."
 #endif
 
 G_BEGIN_DECLS
 
+/**
+ * gtu_skip_if_reached:
+ * @message: (allow-none): explanation of skip.
+ *
+ * Skips and aborts the current test, with an optional message describing why
+ * the test was skipped.
+ *
+ * This function doesn't return to the caller, instead proceeding onto the next
+ * test.
+ */
 void gtu_skip_if_reached (const char* message) G_GNUC_NORETURN;
 
+/**
+ * gtu_skip_if:
+ * @condition: %FALSE for a no-op, %TRUE if this test should be skipped.
+ * @message:   (allow-none): explanation of skip.
+ *
+ * If @condition is truthy, this test is skipped and we abort execution.
+ *
+ * See gtu_skip_if_reached().
+ */
 #define gtu_skip_if(condition, message) G_STMT_START { \
   if (G_UNLIKELY (condition))                          \
     gtu_skip_if_reached (message);                     \
 } G_STMT_END
 
+/**
+ * gtu_skip_if_not_thorough:
+ *
+ * Skip and abort the current test if thorough tests have been disabled, e.g.
+ * with the `--quick` command line flag.
+ */
 #define gtu_skip_if_not_thorough() \
   gtu_skip_if (g_test_quick (), "Thorough tests disabled")
 
+/**
+ * gtu_skip_if_not_perf:
+ *
+ * Skip and abort the current test if performance tests are disabled.
+ */
 #define gtu_skip_if_not_perf() \
   gtu_skip_if (!g_test_perf (), "Performance tests disabled")
 
 
+/**
+ * _gtu_assertion_message: (skip)
+ * @file:       #__FILE__
+ * @line:       #__LINE__
+ * @function:   #G_STRFUNC
+ * @message:    (allow-none): assertion failure explanation.
+ *
+ * Print an error message and abort the current test.
+ *
+ * Do not call this function.
+ */
 void _gtu_assertion_message (const char* file,
                              const char* line,
                              const char* function,
                              const char* message) G_GNUC_NORETURN;
 
+/**
+ * gtu_assert_with_message:
+ * @condition: %TRUE for a no-op, %FALSE if this test has failed and we should
+ *             abort the test.
+ * @message:   (allow-none): explanation of failure if @condition is %FALSE
+ *
+ * If @condition is truthy, do nothing. When @condition is falsy, we abort the
+ * test and print @message with some extra diagnostic information.
+ */
 #define gtu_assert_with_message(condition, message) G_STMT_START {          \
   if (G_LIKELY (condition))                                                 \
     ;                                                                       \
@@ -34,21 +99,58 @@ void _gtu_assertion_message (const char* file,
                             message);                                       \
 } G_STMT_END
 
+/**
+ * gtu_assert_not_reached:
+ *
+ * Marks the current test as failed. Does not return.
+ */
 #define gtu_assert_not_reached() \
   gtu_assert_with_message (false, "code should not be reached")
 
+/**
+ * gtu_assert:
+ * @condition: %TRUE for a no-op, %FALSE if this test has failed and we should
+ *             abort the test.
+ *
+ * If @condition is truthy, do nothing. When @condition is falsy, we abort the
+ * current test.
+ */
 #define gtu_assert(condition) \
   gtu_assert_with_message (condition, "assertion failed: " #condition)
 
+/**
+ * gtu_assert_true:
+ * @expression: expression to be evaluated.
+ *
+ * See gtu_assert().
+ */
 #define gtu_assert_true(expression) \
   gtu_assert_with_message ((expression), "should be true: " #expression)
 
+/**
+ * gtu_assert_false:
+ * @expression: expression to be evaluated.
+ *
+ * Performs the inverse of gtu_assert_true().
+ */
 #define gtu_assert_false(expression) \
   gtu_assert_with_message (!(expression), "should be false: " #expression)
 
+/**
+ * gtu_assert_null:
+ * @expression: expression to be tested.
+ *
+ * Aborts the current test if @expression is not equal to %NULL.
+ */
 #define gtu_assert_null(expression) \
   gtu_assert_with_message ((expression) == NULL, "should be NULL: " #expression)
 
+/**
+ * gtu_assert_nonnull:
+ * @expression: expression to be tested.
+ *
+ * Aborts the current test if @expression is equal to %NULL.
+ */
 #define gtu_assert_nonnull(expression)              \
   gtu_assert_with_message ((expression) != NULL,    \
                            "shouldn't be NULL: " #expression)
--- include/gtu.h
+++ include/gtu.h
@@ -1,6 +1,16 @@
 #ifndef __GII_TEST_UTILS_H__
 #define __GII_TEST_UTILS_H__
 
+/**
+ * SECTION:gtu
+ * @short_description: utilities for the GLib test framework
+ * @title: Gii Test Utilities
+ * @include: gtu.h
+ *
+ * The Gii Test Utilities (or GTU) library provides some wrapper utilities atop
+ * the GLib testing framework.
+ */
+
 #include <stdbool.h>
 #include <glib.h>
 #include <glib-object.h>
@@ -9,21 +19,89 @@
 
 G_BEGIN_DECLS
 
-typedef void (*GtuTestFunc) (void* target);
-
+/**
+ * gtu_init:
+ * @args:        (array length=args_length): an array of arguments passed to
+ *                                           main().
+ * @args_length: the number of elements in @args.
+ *
+ * Initialises the GLib test framework and the GTU library.
+ *
+ * This function may be called several times.
+ *
+ * This function must be called before any other GTU functionality is used.
+ */
 void gtu_init (char** args, int args_length);
 
+/**
+ * gtu_has_initialized:
+ *
+ * Returns %TRUE if GTU has been initialised.
+ *
+ * Returns: %TRUE if gtu_init() has been called successfully, %FALSE otherwise.
+ */
 bool gtu_has_initialized (void);
 
+/**
+ * GtuTestFunc:
+ * @target: (closure): pointer to user data.
+ *
+ * A user-supplied function within which a test case is implemented.
+ */
+typedef void (*GtuTestFunc) (void* target);
+
+/**
+ * gtu_create_test_case:
+ * @name:                the name of the new test case.
+ * @func:                function implementing a test.
+ * @func_target:         (allow-none) (transfer full) (closure):
+ *                       pointer to user data, passed to @func.
+ * @func_target_destroy: (allow-none) (destroy): frees @func_target.
+ *
+ * Creates a new test case from which GTU flow control facilities (e.g.
+ * gtu_skip_if(), gtu_assert()) can be used.
+ *
+ * Returns: a new #GTestCase.
+ */
 GTestCase* gtu_create_test_case (const char* name,
                                  GtuTestFunc func,
                                  void* func_target,
                                  GDestroyNotify func_target_destroy);
 
+#ifndef __GTK_DOC_IGNORE__
 static inline int gtu_run_suite (GTestSuite* suite) {
+  int ret;
+  static bool has_run = false;
+  g_return_val_if_fail (!has_run, 1);
+
   g_test_suite_add_suite (g_test_get_root (), suite);
-  return g_test_run ();
+  ret = g_test_run ();
+  has_run = true;
+  return ret;
 }
+#endif
+
+/* hack around gtk-doc */
+#define __NOT_GTK_DOC__
+#ifndef __NOT_GTK_DOC__
+
+/**
+ * gtu_run_suite:
+ * @suite: a GLib test suite to be executed.
+ *
+ * Convenience function that adds @suite to the global #GTestSuite and calls
+ * g_test_run().
+ *
+ * No further tests may be run (since g_test_run() may only be called once), so
+ * any tests that should be run must be added to @suite before calling this
+ * function.
+ *
+ * Returns: result from g_test_run().
+ */
+int gtu_run_suite (GTestSuite* suite);
+
+#endif
+#undef __NOT_GTK_DOC__
 
 G_END_DECLS
 

Bob131/giitestutils

commit 6fba8ff797bca24faac3c8dc346f68cdffeb34b0

src: clean up logging
log.c was starting to get pretty messy, so we break it up into several
files and give it a proper well-defined interface.

src/log/tap.c
@@ -1,0 +1,147 @@
+/* Implementation of logio.h */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "priv.h"
+#include "logio.h"
+#include "log-color.h"
+
+static volatile unsigned test_count = 0;
+static unsigned expected_tests = 0;
+G_LOCK_DEFINE_STATIC (stdout);
+
+static const char* diagnostic (void) {
+  return gtu_log_supports_color () ?
+    "\033[1m#\033[0m " : /* bolded '# ' */
+    "# ";
+}
+
+static void log_vprintf (const char* format, va_list args) {
+  G_LOCK (stdout);
+  vfprintf (stdout, format, args);
+  G_UNLOCK (stdout);
+}
+
+static void log_printf (const char* format, ...) {
+  va_list args;
+  va_start (args, format);
+  log_vprintf (format, args);
+}
+
+static void diag_vprintf (const char* format, va_list args) {
+  int i;
+  char buf;
+
+  char* message = g_strdup_vprintf (format, args);
+
+  G_LOCK (stdout);
+
+  /* To be TAP-compliant, lines written to stdout that aren't test results must
+     be prefixed with '#'. We iterate through `message', printing "# " after
+     every newline unless it's a trailing newline. */
+  fprintf (stdout, diagnostic ());
+  for (i = 0, buf = '\0'; message[i] != '\0'; fputc (buf, stdout), i++) {
+    if (buf == '\n')
+      fprintf (stdout, "%s  ", diagnostic ()); /* add some indenting */
+    buf = message[i];
+  }
+
+  if (buf != '\n')
+    fputc ('\n', stdout);
+
+  G_UNLOCK (stdout);
+
+  g_free (message);
+}
+
+static void log_test_plan (unsigned n_tests, bool disable) {
+  static volatile size_t expected_tests__volatile = 0;
+
+  if (g_once_init_enter (&expected_tests__volatile)) {
+    if (disable) {
+      g_once_init_leave (&expected_tests__volatile, 1);
+      return;
+    }
+
+    if (n_tests == 0) {
+      log_printf ("1..0 # Skipped: no tests to run\n");
+      exit (0);
+    }
+
+    log_printf ("1..%u\n", n_tests);
+    expected_tests = n_tests;
+
+    g_once_init_leave (&expected_tests__volatile, n_tests);
+  }
+}
+
+void gtu_log_disable_test_plan (void) {
+  log_test_plan (0, true);
+}
+
+void gtu_log_test_plan (unsigned n_tests) {
+  log_test_plan (n_tests, false);
+}
+
+void gtu_log_diagnostic (const char* format, ...) {
+  va_list args;
+
+  g_return_if_fail (format != NULL);
+
+  va_start (args, format);
+  diag_vprintf (format, args);
+}
+
+void gtu_log_bail_out (const char* format, ...) {
+  va_list args;
+
+  G_LOCK (stdout);
+
+  fprintf (stdout, "Bail out!");
+
+  if (format != NULL) {
+    fputc (' ', stdout);
+    va_start (args, format);
+    vfprintf (stdout, format, args);
+  }
+
+  fputc ('\n', stdout);
+
+  /* exit status to signal an error to the automake harness */
+  exit (99);
+}
+
+static void log_test_result (bool success,
+                             const char* test_description,
+                             const char* tap_directive,
+                             const char* user_directive)
+{
+  bool has_directive = tap_directive != NULL || user_directive != NULL;
+  unsigned prev_test_count = g_atomic_int_add (&test_count, 1);
+  log_printf ("%sok %u %s%s%s%s\n",
+              success ? "" : "not ",
+              prev_test_count + 1,
+              test_description != NULL ? test_description : "-",
+              has_directive ? " # " : "",
+              tap_directive != NULL ? tap_directive : "",
+              user_directive != NULL ? user_directive : "");
+}
+
+void gtu_log_test_success (const char* test_description,
+                           const char* directive)
+{
+  log_test_result (true, test_description, NULL, directive);
+}
+
+void gtu_log_test_skipped (const char* test_description,
+                           const char* directive)
+{
+  log_test_result (true, test_description, "SKIP", directive);
+}
+
+void gtu_log_test_failed (const char* test_description,
+                          const char* directive)
+{
+  log_test_result (false, test_description, NULL, directive);
+}

src/Makefile.am
@@ -6,12 +6,20 @@ else
 noinst_LIBRARIES = libgtu.a
 endif
 
+BUILT_SOURCES = log/color-table.c.in
+
+log/color-table.c.in:
+	$(AM_V_GEN) log/generate-color-table.py > $@
+
 libgtu_a_SOURCES = \
 	init.c \
-	log.c \
 	flags.c \
 	path.c \
 	object.c \
+	log/tap.c \
+	log/color.c \
+	log/glib-handlers.c \
+	log/glib-format.c \
 	test-case/test-case.c \
 	test-case/run.c \
 	test-suite/test-suite.c \

src/gtu-priv.h
@@ -52,18 +52,6 @@ G_GNUC_INTERNAL bool _gtu_test_case_has_run (GtuTestCase* self);
 /* returns array of GtuTestObject */
 G_GNUC_INTERNAL GPtrArray* _gtu_test_case_get_deps (GtuTestCase* self);
 
-/* Returns `true' if `self' handled the message, `false' otherwise. The
-   `caller' parameter should be the address of the function acting as a GLib
-   log handler. */
-G_GNUC_INTERNAL bool _gtu_test_case_handle_message (GtuTestCase* self,
-                                                    const char* domain,
-                                                    GLogLevelFlags level,
-                                                    const char* message,
-                                                    uintptr_t caller);
-
-/* The test currently being executed. May be NULL */
-G_GNUC_INTERNAL extern GtuTestCase* _gtu_current_test;
-
 typedef struct {
   GList* path_selectors;
   GList* path_skippers;

src/init.c
@@ -1,7 +1,9 @@
 #include <string.h>
 #include <stdio.h>
 #include "gtu-priv.h"
-#include "log.h"
+
+#include "log/logio.h"
+#include "log/log-glib.h"
 
 #ifdef HAVE_ALLOCA_H
 # include <alloca.h>
@@ -128,6 +130,7 @@ static bool parse_args (char** args, int args_length) {
 
     } else if (strcmp (args[i], "-l") == 0) {
       _test_mode.list_only = true;
+      gtu_log_disable_test_plan ();
 
     } else if (GET_ARG ("--GTestLogFD")     ||
                GET_ARG ("--GTestSkipCount") ||
@@ -140,8 +143,8 @@ static bool parse_args (char** args, int args_length) {
 
   /* this shouldn't necessarily be fatal, so just log */
   if (!tap_set && !_test_mode.list_only)
-    _gtu_log_printf ("WARNING: non-TAP test logging is unsupported. %s\n",
-                     "Run with --tap");
+    gtu_log_diagnostic ("WARNING: non-TAP test logging is unsupported. %s\n",
+                        "Run with --tap");
 
   return tap_set;
 }
@@ -190,7 +193,7 @@ void gtu_init (char** args, int args_length) {
       G_N_ELEMENTS (_glib_debug_keys)
     );
 
-    _gtu_install_glib_loggers ();
+    gtu_log_g_install_handlers ();
 
     _has_initialized = true;
   }

src/log.c
@@ -1,414 +1,0 @@
-#define _POSIX_C_SOURCE 201111L
-#include <stdio.h>
-#include <string.h>
-#include "log.h"
-
-/* TAP-compliant GLib message handling */
-
-static bool has_logged_plan = false;
-
-static bool should_log (GLogLevelFlags log_level) {
-  if (log_level & G_LOG_FLAG_FATAL)
-    return true;
-
-  switch (log_level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:
-    case G_LOG_LEVEL_CRITICAL:
-    case G_LOG_LEVEL_WARNING:
-      return true;
-
-    case G_LOG_LEVEL_MESSAGE:
-      return !(gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_QUIET);
-
-    case G_LOG_LEVEL_INFO:
-    case G_LOG_LEVEL_DEBUG:
-      return gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_VERBOSE;
-
-    default:
-      g_assert_not_reached ();
-  }
-}
-
-/* we probably don't want to be calling isatty() all the time for something
-   trivial, so we wrap it in a once_init */
-static bool supports_color (void) {
-  /* -1 for unsupported, +1 for supported */
-  static volatile gssize supported = 0;
-
-  if (g_once_init_enter (&supported)) {
-    gssize result = g_log_writer_supports_color (fileno (stdout)) ? +1 : -1;
-    g_once_init_leave (&supported, result);
-  }
-
-  return supported > 0 ? true : false;
-}
-
-static const char* level_color (GLogLevelFlags level) {
-  if (!supports_color ())
-    return "";
-
-  if (level & G_LOG_FLAG_FATAL)
-    level = G_LOG_LEVEL_ERROR;
-
-  switch (level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:     return "\033[1;31m";
-    case G_LOG_LEVEL_CRITICAL:  return "\033[1;35m";
-    case G_LOG_LEVEL_WARNING:   return "\033[1;33m";
-    case G_LOG_LEVEL_MESSAGE:   return "\033[1;32m";
-    case G_LOG_LEVEL_INFO:      return "\033[1;34m";
-    case G_LOG_LEVEL_DEBUG:     return "\033[1;36m";
-  }
-
-  return "";
-}
-
-static const char* color_reset (void) {
-  return supports_color () ? "\033[0m" : "";
-}
-
-static const char* diagnostic (void) {
-  static char* string = NULL;
-
-  if (string == NULL)
-    string = g_strdup_printf ("%s#%s ",
-                              supports_color () ? "\033[1m" : "",
-                              color_reset ());
-
-  return string;
-}
-
-static const char* level_to_string (GLogLevelFlags log_level) {
-#define ret(str) {                    \
-  if (log_level & G_LOG_FLAG_FATAL) { \
-    return "FATAL-" str;              \
-  } else {                            \
-    return str;                       \
-  }                                   \
-}
-
-  switch (log_level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:
-      ret ("ERROR");
-
-    case G_LOG_LEVEL_CRITICAL:
-      ret ("CRITICAL");
-
-    case G_LOG_LEVEL_WARNING:
-      ret ("WARNING");
-
-    case G_LOG_LEVEL_MESSAGE:
-      ret ("MESSAGE");
-
-    case G_LOG_LEVEL_INFO:
-      ret ("INFO");
-
-    case G_LOG_LEVEL_DEBUG:
-      ret ("DEBUG");
-
-    default:
-      g_assert_not_reached ();
-  }
-
-#undef ret
-}
-
-/* `message' may be NULL. If non-NULL, it should contain a trailing new line */
-static void bail_out (GLogLevelFlags level, const char* message) {
-  GtuDebugFlags debug_flags = _gtu_debug_flags_get ();
-
-  fprintf (stdout, "%sBail out!%s", level_color (level), color_reset ());
-  if (message != NULL)
-    fprintf (stdout, " %s", message);
-  else
-    fprintf (stdout, "\n");
-
-  if ((level & G_LOG_LEVEL_WARNING &&
-       debug_flags & GTU_DEBUG_FLAGS_FATAL_WARNINGS) ||
-      (level & G_LOG_LEVEL_CRITICAL &&
-       debug_flags & GTU_DEBUG_FLAGS_FATAL_CRITICALS))
-  {
-    g_abort ();
-  }
-
-  /* exit status to signal an error to the automake harness */
-  exit (99);
-}
-
-static void glib_test_logger (const char* log_domain, GLogLevelFlags log_level,
-                              const char* message, void* data)
-{
-  bool suppress = false;
-
-  (void) data;
-
-  if (_gtu_current_test != NULL &&
-      _gtu_test_case_handle_message (_gtu_current_test,
-                                     log_domain,
-                                     log_level,
-                                     message,
-                                     (uintptr_t) &glib_test_logger))
-  {
-    suppress = true;
-    if (should_log (G_LOG_LEVEL_INFO))
-      goto do_log;
-    else
-      return;
-  }
-
-  if (should_log (log_level)) {
-    char *fmtmsg;
-
-do_log:
-    fmtmsg = _gtu_log_format_message (log_domain, log_level, message);
-
-    if (suppress) {
-      char* smsg = g_strconcat ("Suppressed message: ", fmtmsg, NULL);
-      g_free (fmtmsg);
-      fmtmsg = _gtu_log_format_message (GTU_LOG_DOMAIN, G_LOG_LEVEL_INFO, smsg);
-      g_free (smsg);
-    }
-
-    if (log_level & G_LOG_FLAG_FATAL && !suppress) {
-      bail_out (log_level, fmtmsg);
-
-    } else {
-      _gtu_log_printf (fmtmsg);
-      g_free (fmtmsg);
-    }
-  }
-}
-
-static GLogWriterOutput glib_structured_logger (GLogLevelFlags log_level,
-                                                const GLogField* fields,
-                                                size_t n_fields,
-                                                void* data)
-{
-  size_t i;
-  bool suppress = false;
-
-  (void) data;
-
-  if (_gtu_current_test != NULL) {
-    const char* message = NULL;
-    const char* domain = NULL;
-
-    uintptr_t us = (uintptr_t) &glib_structured_logger;
-
-    for (i = 0; i < n_fields && (message == NULL || domain == NULL); i++) {
-      const char** var;
-
-      if (strcmp (fields[i].key, "MESSAGE") == 0) {
-        var = &message;
-      } else if (strcmp (fields[i].key, "GLIB_DOMAIN")) {
-        var = &domain;
-      } else {
-        continue;
-      }
-
-      g_return_val_if_fail (fields[i].length == -1, G_LOG_WRITER_UNHANDLED);
-      *var = (const char*) &fields[i].value;
-    }
-
-    if (message != NULL && _gtu_test_case_handle_message (_gtu_current_test,
-                                                          domain,
-                                                          log_level,
-                                                          message,
-                                                          us))
-    {
-      suppress = true;
-      if (should_log (G_LOG_LEVEL_INFO))
-        goto do_log;
-      else
-        goto ret;
-    }
-  }
-
-  if (!should_log (log_level))
-    goto ret;
-
-do_log:
-  _gtu_log_printf ("%s: new structured message:\n",
-                   level_to_string (log_level));
-
-  for (i = 0; i < n_fields; i++) {
-    char* value = NULL;
-
-    if (fields[i].length == -1)
-      value = g_strescape ((const char*) fields[i].value, "\t'\"");
-
-    _gtu_log_printf ("  '%s': %s\n",
-                     fields[i].key,
-                     value != NULL ? value : "<binary data>");
-
-    if (value != NULL)
-      g_free (value);
-  }
-
-  if (log_level & G_LOG_FLAG_FATAL && !suppress) {
-    bail_out (log_level, NULL);
-  }
-
-ret:
-  return G_LOG_WRITER_HANDLED;
-}
-
-static gboolean fatal_log_handler (const char* log_domain,
-                                   GLogLevelFlags log_level,
-                                   const char* message,
-                                   void* data)
-{
-  (void) log_level;
-  (void) message;
-  (void) data;
-
-  /* Fatal logs from us indicate a problem with the user's test suite.
-     Otherwise we never want GLib to abort for us, as we handle this
-     ourselves. */
-  return g_strcmp0 (log_domain, GTU_LOG_DOMAIN) == 0;
-}
-
-static void log_empty_test_plan (void) {
-  if (has_logged_plan || _gtu_get_test_mode ()->list_only)
-    return;
-
-  fprintf (stdout, "1..0 # Skipped: no tests to run\n");
-}
-
-static void glib_print_handler (const char* message) {
-  int i;
-  char buf;
-
-  /* To be TAP-compliant, lines written to stdout that aren't test results must
-     be prefixed with '#'. We iterate through `message', printing "# " after
-     every newline unless it's a trailing newline. */
-  for (i = 0, buf = '\n'; message[i] != '\0'; fputc (buf, stdout), i++) {
-    if (buf == '\n')
-      fprintf (stdout, diagnostic ());
-    buf = message[i];
-  }
-
-  if (buf != '\n')
-    fprintf (stdout, "\n");
-}
-
-static void glib_printerr_handler (const char* message) {
-  char* new_message = g_strconcat ("STDERR: ", message, NULL);
-  glib_print_handler (new_message);
-  g_free (new_message);
-}
-
-void _gtu_install_glib_loggers (void) {
-  g_set_print_handler (&glib_print_handler);
-  g_set_printerr_handler (&glib_printerr_handler);
-
-  g_log_set_default_handler (glib_test_logger, NULL);
-  g_log_set_writer_func (glib_structured_logger, NULL, NULL);
-
-  g_test_log_set_fatal_handler (&fatal_log_handler, NULL);
-
-  atexit (&log_empty_test_plan);
-}
-
-void _gtu_log_printf (const char* format, ...) {
-  va_list args;
-  char* message;
-
-  va_start (args, format);
-  message = g_strdup_vprintf (format, args);
-  glib_print_handler (message);
-  g_free (message);
-}
-
-void _gtu_log_test_plan (unsigned n_tests) {
-  g_assert (!has_logged_plan);
-
-  if (!_gtu_get_test_mode ()->list_only) {
-    if (n_tests == 0)
-      log_empty_test_plan ();
-    else
-      fprintf (stdout, "1..%u\n", n_tests);
-  }
-
-  has_logged_plan = true;
-}
-
-void _gtu_log_test_result (GtuTestResult result,
-                           const char* path,
-                           const char* message)
-{
-  static unsigned test_number = 1;
-
-  g_assert (path != NULL);
-
-  switch (result) {
-    case GTU_TEST_RESULT_PASS:
-    case GTU_TEST_RESULT_SKIP:
-      fprintf (stdout, "ok");
-      break;
-
-    case GTU_TEST_RESULT_FAIL:
-      fprintf (stdout, "not ok");
-      break;
-
-    default:
-      g_assert_not_reached ();
-  }
-
-  fprintf (stdout, " %d ", test_number++);
-  fprintf (stdout, path);
-
-  if (result == GTU_TEST_RESULT_PASS && message == NULL) {
-    fprintf (stdout, "\n");
-    return;
-  }
-
-  fprintf (stdout, " # ");
-
-  switch (result) {
-    case GTU_TEST_RESULT_PASS:
-      fprintf (stdout, "PASS");
-      break;
-
-    case GTU_TEST_RESULT_SKIP:
-      fprintf (stdout, "SKIP");
-      break;
-
-    case GTU_TEST_RESULT_FAIL:
-      fprintf (stdout, "FAIL");
-      break;
-
-    default:
-      g_assert_not_reached ();
-  }
-
-  if (message != NULL)
-    fprintf (stdout, " %s", message);
-
-  fprintf (stdout, "\n");
-}
-
-char* _gtu_log_format_message (const char* domain,
-                               GLogLevelFlags level,
-                               const char* message)
-{
-  const char *domain_head = "",
-             *domain_str  = "",
-             *domain_tail = "";
-
-  if (domain != NULL && domain[0] != '\0') {
-    domain_head = "(**";
-    domain_str = domain;
-    domain_tail = ") ";
-  }
-
-  return g_strdup_printf ("%s%s%s: %s%s%s%s",
-                          level_color (level),
-                          level_to_string (level),
-                          color_reset (),
-
-                          domain_head,
-                          domain_str,
-                          domain_tail,
-
-                          message);
-}

src/log.h
@@ -1,25 +1,0 @@
-#ifndef __GII_TEST_UTILS_TEST_LOG_PRIV_H__
-#define __GII_TEST_UTILS_TEST_LOG_PRIV_H__
-
-#include "gtu-priv.h"
-
-G_GNUC_INTERNAL void _gtu_install_glib_loggers (void);
-
-/* log raw message without any additional formatting beyond what's needed to
-   make it safe for the test harness */
-G_GNUC_INTERNAL void _gtu_log_printf (const char* format, ...);
-
-/* if `n_tests' is zero, logs an empty test plan marked 'skip' */
-G_GNUC_INTERNAL void _gtu_log_test_plan (unsigned n_tests);
-
-/* `message' may be NULL */
-G_GNUC_INTERNAL void _gtu_log_test_result (GtuTestResult result,
-                                           const char* path,
-                                           const char* message);
-
-/* No trailing newline. Free the result when done */
-G_GNUC_INTERNAL char* _gtu_log_format_message (const char* domain,
-                                               GLogLevelFlags level,
-                                               const char* message);
-
-#endif

src/log/color.c
@@ -1,0 +1,38 @@
+#define _POSIX_C_SOURCE 201111L
+#include <stdio.h>
+#include <stdint.h>
+
+#include "priv.h"
+#include "log-color.h"
+
+/* we probably don't want to be calling isatty() all the time for something
+   trivial, so we wrap it in a once_init */
+bool gtu_log_supports_color (void) {
+  /* -1 for unsupported, +1 for supported */
+  static volatile gssize supported = 0;
+
+  if (g_once_init_enter (&supported)) {
+    gssize result = g_log_writer_supports_color (fileno (stdout)) ? +1 : -1;
+    g_once_init_leave (&supported, result);
+  }
+
+  return supported > 0 ? true : false;
+}
+
+const char* gtu_log_lookup_color (GtuLogColor color) {
+  if (!gtu_log_supports_color ())
+    return "";
+
+  switch ((uint16_t) color) {
+    case GTU_LOG_COLOR_DISABLE: return "\033[0m";
+
+    case GTU_LOG_COLOR_FLAG_BOLD | GTU_LOG_COLOR_FLAG_UNDERSCORE:  return "\033[1;4m";
+    case GTU_LOG_COLOR_FLAG_BOLD | GTU_LOG_COLOR_FLAG_BLINK:       return "\033[1;4m";
+    case GTU_LOG_COLOR_FLAG_BLINK | GTU_LOG_COLOR_FLAG_UNDERSCORE: return "\033[5;4m";
+
+#   include "color-table.c.in"
+
+    default:
+      g_assert_not_reached ();
+  }
+}

commit 9fb50811a63b02acf1d1350bc6145c0b42c97a3f

src/log/tap: fix directive regression
Emits a space between TAP directives and user directives.

Fixes: 6fba8ff797bca24faac3c8dc346f68cdffeb34b0

src/log/tap.c
@@ -122,12 +122,13 @@ static void log_test_result (bool success,
 {
   bool has_directive = tap_directive != NULL || user_directive != NULL;
   unsigned prev_test_count = g_atomic_int_add (&test_count, 1);
-  log_printf ("%sok %u %s%s%s%s\n",
+  log_printf ("%sok %u %s%s%s%s%s\n",
               success ? "" : "not ",
               prev_test_count + 1,
               test_description != NULL ? test_description : "-",
               has_directive ? " # " : "",
               tap_directive != NULL ? tap_directive : "",
+              tap_directive != NULL && user_directive != NULL ? " " : "",
               user_directive != NULL ? user_directive : "");
 }
 
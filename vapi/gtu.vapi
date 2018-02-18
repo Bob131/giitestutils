Bob131/giitestutils

commit ec85065b546216f7fbaa1cf4c6ca528b50adb73f

add more assert features
- Splits gtu_assert* and gtu_skip* functions/macros into a new
  gtu-asserts.h header

- Adds assertion diagnostics (printing the file/line/function on assert
  failure)

- Adds extra gtu_assert macros (gtu_assert_false, gtu_assert_null, etc)

vapi/gtu.vapi
@@ -1,10 +1,26 @@
+[NoReturn]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_not_reached")]
+public void assert_not_reached ();
+
 [Assert]
 [CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
 public void assert (bool expr);
 
-[NoReturn]
-[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_not_reached")]
-public void assert_not_reached ();
+[Assert]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+public void assert_true (bool expr);
+
+[Assert]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+public void assert_false (bool expr);
+
+[Assert]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+public void assert_null (...);
+
+[Assert]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+public void assert_nonnull (...);
 
 [CCode (cheader_filename = "gtu.h")]
 namespace Gtu {

include/gtu-asserts.h
@@ -1,0 +1,58 @@
+#ifndef __GII_TEST_UTILS_ASSERTS_H__
+#define __GII_TEST_UTILS_ASSERTS_H__
+
+#ifndef __GII_TEST_UTILS_H__
+#error "Only <gtu.h> can be included directly."
+#endif
+
+G_BEGIN_DECLS
+
+void gtu_skip_if_reached (const char* message) G_GNUC_NORETURN;
+
+#define gtu_skip_if(condition, message) G_STMT_START { \
+  if (G_UNLIKELY (condition))                          \
+    gtu_skip_if_reached (message);                     \
+} G_STMT_END
+
+#define gtu_skip_if_not_thorough() \
+  gtu_skip_if (g_test_quick (), "Thorough tests disabled")
+
+#define gtu_skip_if_not_perf() \
+  gtu_skip_if (!g_test_perf (), "Performance tests disabled")
+
+
+void _gtu_assertion_message (const char* file,
+                             const char* line,
+                             const char* function,
+                             const char* message) G_GNUC_NORETURN;
+
+#define gtu_assert_with_message(condition, message) G_STMT_START {          \
+  if (G_LIKELY (condition))                                                 \
+    ;                                                                       \
+  else                                                                      \
+    _gtu_assertion_message (__FILE__, G_STRINGIFY (__LINE__), G_STRFUNC,    \
+                            message);                                       \
+} G_STMT_END
+
+#define gtu_assert_not_reached() \
+  gtu_assert_with_message (false, "code should not be reached")
+
+#define gtu_assert(condition) \
+  gtu_assert_with_message (condition, "assertion failed: " #condition)
+
+#define gtu_assert_true(expression) \
+  gtu_assert_with_message ((expression), "should be true: " #expression)
+
+#define gtu_assert_false(expression) \
+  gtu_assert_with_message (!(expression), "should be false: " #expression)
+
+#define gtu_assert_null(expression) \
+  gtu_assert_with_message ((expression) == NULL, "should be NULL: " #expression)
+
+#define gtu_assert_nonnull(expression)              \
+  gtu_assert_with_message ((expression) != NULL,    \
+                           "shouldn't be NULL: " #expression)
+
+G_END_DECLS
+
+#endif

include/gtu.h
@@ -5,31 +5,15 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include "gtu-asserts.h"
+
 G_BEGIN_DECLS
-
-#define gtu_assert(cond) G_STMT_START { \
-  if (G_UNLIKELY (!(cond)))             \
-    gtu_assert_not_reached ();          \
-} G_STMT_END
-
-#define gtu_skip_if(cond, message) G_STMT_START { \
-  if (G_UNLIKELY (cond))                          \
-    gtu_skip_if_reached (message);                \
-} G_STMT_END
-
-#define gtu_skip_if_not_thorough() \
-  gtu_skip_if (g_test_quick (), "Thorough tests disabled")
-
-#define gtu_skip_if_not_perf() \
-  gtu_skip_if (!g_test_perf (), "Performance tests disabled")
 
 typedef void (*GtuTestFunc) (void* target);
 
 void gtu_init (char** args, int args_length);
-bool gtu_has_initialized (void);
 
-void gtu_assert_not_reached (void)                G_GNUC_NORETURN;
-void gtu_skip_if_reached    (const char* message) G_GNUC_NORETURN;
+bool gtu_has_initialized (void);
 
 GTestCase* gtu_create_test_case (const char* name,
                                  GtuTestFunc func,

src/gtu-test.c
@@ -2,6 +2,10 @@
 
 /* a little demo program */
 
+#define ASSERT_TEST(suite, test)                                            \
+  g_test_suite_add ((suite), gtu_create_test_case (#test, test ## _test,    \
+                                                   NULL, NULL))
+
 static void assert_test (void* data) {
   (void) data;
   gtu_assert (TRUE);
@@ -9,10 +13,45 @@ static void assert_test (void* data) {
   g_assert_not_reached ();
 }
 
+static void assert_not_reached_test (void* data){
+  (void) data;
+  gtu_assert_not_reached ();
+  g_assert_not_reached ();
+}
+
+static void assert_true_test (void* data) {
+ (void) data;
+ gtu_assert_true (TRUE);
+ gtu_assert_true (FALSE);
+ g_assert_not_reached ();
+}
+
+static void assert_false_test (void* data) {
+  (void) data;
+  gtu_assert_false (FALSE);
+  gtu_assert_false (TRUE);
+  g_assert_not_reached ();
+}
+
+static void assert_null_test (void* data) {
+  (void) data;
+  gtu_assert_null (NULL);
+  gtu_assert_null ((void*) 34);
+  g_assert_not_reached ();
+}
+
+static void assert_nonnull_test (void* data) {
+  (void) data;
+  gtu_assert_nonnull ((void*) 23);
+  gtu_assert_nonnull (NULL);
+  g_assert_not_reached ();
+}
+
 static void skip_test (void* data) {
   (void) data;
   gtu_skip_if_not_thorough ();
   gtu_skip_if_not_perf ();
+  gtu_skip_if (TRUE, NULL);
   g_assert_not_reached ();
 }
 
@@ -31,8 +70,13 @@ int main (int argc, char* argv[]) {
 
   suite = g_test_create_suite ("gtu-test");
 
-  g_test_suite_add (suite, gtu_create_test_case ("assert", assert_test,
-                                                 NULL, NULL));
+  ASSERT_TEST (suite, assert);
+  ASSERT_TEST (suite, assert_not_reached);
+  ASSERT_TEST (suite, assert_true);
+  ASSERT_TEST (suite, assert_false);
+  ASSERT_TEST (suite, assert_null);
+  ASSERT_TEST (suite, assert_nonnull);
+
   g_test_suite_add (suite, gtu_create_test_case ("skip", skip_test,
                                                  NULL, _destroy));
   g_test_suite_add (suite, gtu_create_test_case ("destroy", destroy_test,

src/test-case.c
@@ -32,8 +32,18 @@ static TestCaseContext* _current_tc_context = NULL;
 } G_STMT_END
 
 
-void gtu_assert_not_reached (void) {
+void _gtu_assertion_message (const char* file,
+                             const char* line,
+                             const char* function,
+                             const char* message)
+{
   CURRENT_CONTEXT_CHECK ();
+
+  g_assert (file != NULL && line != NULL && function != NULL);
+  if (message == NULL)
+    message = "assert failed";
+
+  g_printerr ("**\nERROR:%s:%s:%s: %s\n", file, line, function, message);
 
   if (_gtu_get_debug_flags () & GTU_DEBUG_FATAL_ASSERTS)
     abort ();

commit f2d75376c7e084760fa37bfccff0c3fdae4fe134

vapi: fix really silly bug
I only just realised that the cnames for most of the asserts are
completely wrong.

Fixes: ec85065b546216f7fbaa1cf4c6ca528b50adb73f

vapi/gtu.vapi
@@ -5,16 +5,16 @@ public void assert_not_reached ();
 [CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
 public void assert (bool expr);
 
-[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_true")]
 public void assert_true (bool expr);
 
-[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_false")]
 public void assert_false (bool expr);
 
-[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_null")]
 public void assert_null (...);
 
-[CCode (cheader_filename = "gtu.h", cname = "gtu_assert")]
+[CCode (cheader_filename = "gtu.h", cname = "gtu_assert_nonnull")]
 public void assert_nonnull (...);
 
 [CCode (cheader_filename = "gtu.h")]
Bob131/giitestutils

commit 6fba8ff797bca24faac3c8dc346f68cdffeb34b0

src: clean up logging
log.c was starting to get pretty messy, so we break it up into several
files and give it a proper well-defined interface.

src/init.c
@@ -1,7 +1,9 @@
 #include <string.h>
 #include <stdio.h>
 #include "gtu-priv.h"
-#include "log.h"
+
+#include "log/logio.h"
+#include "log/log-glib.h"
 
 #ifdef HAVE_ALLOCA_H
 # include <alloca.h>
@@ -128,6 +130,7 @@ static bool parse_args (char** args, int args_length) {
 
     } else if (strcmp (args[i], "-l") == 0) {
       _test_mode.list_only = true;
+      gtu_log_disable_test_plan ();
 
     } else if (GET_ARG ("--GTestLogFD")     ||
                GET_ARG ("--GTestSkipCount") ||
@@ -140,8 +143,8 @@ static bool parse_args (char** args, int args_length) {
 
   /* this shouldn't necessarily be fatal, so just log */
   if (!tap_set && !_test_mode.list_only)
-    _gtu_log_printf ("WARNING: non-TAP test logging is unsupported. %s\n",
-                     "Run with --tap");
+    gtu_log_diagnostic ("WARNING: non-TAP test logging is unsupported. %s\n",
+                        "Run with --tap");
 
   return tap_set;
 }
@@ -190,7 +193,7 @@ void gtu_init (char** args, int args_length) {
       G_N_ELEMENTS (_glib_debug_keys)
     );
 
-    _gtu_install_glib_loggers ();
+    gtu_log_g_install_handlers ();
 
     _has_initialized = true;
   }

src/log/glib-handlers.c
@@ -1,0 +1,273 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "priv.h"
+#include "logio.h"
+#include "log-glib.h"
+
+#include "gtu.h"
+
+G_LOCK_DEFINE_STATIC (suppress_info);
+static struct {
+  GtuLogGSuppressFunc func;
+  const void* data;
+} suppress_info = {NULL, NULL};
+
+#define SHOULD_SUPPRESS(domain, level, message, func) \
+  (should_suppress ((domain), (level), (message), (uintptr_t) (func)))
+
+static bool should_suppress (const char* domain,
+                             GLogLevelFlags level,
+                             const char* message,
+                             uintptr_t log_entry)
+{
+  bool ret;
+
+  /* TODO: hold read locks. The suppression mechanism is exclusively for the
+     benefit of GtuTestCase, which does all sorts of nasty stack unwinding
+     trickery. If we naively lock the mutex, GtuTestCase will destroy our
+     internal state. */
+
+  /* G_LOCK (suppress_info); */
+
+  ret = suppress_info.func != NULL ?
+    suppress_info.func (domain,
+                        level,
+                        message,
+                        log_entry,
+                        (void*) suppress_info.data) :
+    false;
+
+  /* G_UNLOCK (suppress_info); */
+
+  return ret;
+}
+
+static bool should_log (GLogLevelFlags log_level) {
+  if (log_level & G_LOG_FLAG_FATAL)
+    return true;
+
+  switch (log_level & G_LOG_LEVEL_MASK) {
+    case G_LOG_LEVEL_ERROR:
+    case G_LOG_LEVEL_CRITICAL:
+    case G_LOG_LEVEL_WARNING:
+      return true;
+
+    case G_LOG_LEVEL_MESSAGE:
+      return !(gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_QUIET);
+
+    case G_LOG_LEVEL_INFO:
+    case G_LOG_LEVEL_DEBUG:
+      return gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_VERBOSE;
+
+    default:
+      g_assert_not_reached ();
+  }
+}
+
+static void stdfd_handler (const char* message) {
+  gtu_log_diagnostic (message);
+}
+
+static void message_printer (const char* domain,
+                             GLogLevelFlags level,
+                             const char* message)
+{
+  char* formatted_message = gtu_log_g_format_message (domain, level, message);
+  gtu_log_diagnostic (formatted_message);
+  g_free (formatted_message);
+}
+
+static void message_bailout (const char* domain,
+                             GLogLevelFlags level,
+                             const char* message)
+{
+  char* formatted_message = gtu_log_g_format_message (domain, level, message);
+  gtu_log_bail_out (formatted_message);
+}
+
+static void message_handler (const char* domain,
+                             GLogLevelFlags level,
+                             const char* message,
+                             void* data)
+{
+  (void) data;
+
+  if (SHOULD_SUPPRESS (domain, level, message, &message_handler)) {
+    GString* formatted_message;
+
+    if (!should_log (G_LOG_LEVEL_INFO))
+      return;
+
+    formatted_message = g_string_new ("Suppressed message: ");
+    gtu_log_g_format_message_append (formatted_message,
+                                     domain,
+                                     level,
+                                     message);
+
+    message_printer (GTU_LOG_DOMAIN, G_LOG_LEVEL_INFO, formatted_message->str);
+
+    g_string_free (formatted_message, true);
+
+  } else {
+    if (!should_log (level))
+      return;
+
+    if (level & G_LOG_FLAG_FATAL)
+      message_bailout (domain, level, message);
+    else
+      message_printer (domain, level, message);
+  }
+}
+
+static GLogWriterOutput structured_handler (GLogLevelFlags level,
+                                            const GLogField* fields,
+                                            size_t n_fields,
+                                            void* data)
+{
+  size_t i;
+  const char* domain = NULL;
+  const char* message = NULL;
+  GString* formatted_message;
+  bool suppressed = false;
+
+  (void) data;
+
+  {
+    /* collect the domain/message fields and check suppression */
+    GLogLevelFlags check_level = level;
+
+    for (i = 0; i < n_fields && (message == NULL || domain == NULL); i++) {
+      const char** var;
+
+      if (strcmp (fields[i].key, "MESSAGE") == 0) {
+        var = &message;
+      } else if (strcmp (fields[i].key, "GLIB_DOMAIN") == 0) {
+        var = &domain;
+      } else {
+        continue;
+      }
+
+      g_return_val_if_fail (fields[i].length == -1, G_LOG_WRITER_UNHANDLED);
+      *var = (const char*) fields[i].value;
+    }
+
+    /* call SHOULD_SUPPRESS before we allocate any memory */
+    if (message != NULL &&
+        SHOULD_SUPPRESS (domain, level, message, &structured_handler))
+    {
+      check_level = G_LOG_LEVEL_INFO;
+      suppressed = true;
+    }
+
+    if (!should_log (check_level))
+      goto ret;
+  }
+
+  formatted_message = g_string_new (!suppressed ?
+                                    "Structured message:" :
+                                    "Structured message (suppressed):");
+
+  if (message != NULL) {
+    g_string_append_c (formatted_message, ' ');
+    gtu_log_g_format_message_append (formatted_message,
+                                     domain, level, message);
+
+    if (suppressed) {
+      char* info_message = gtu_log_g_format_message (GTU_LOG_DOMAIN,
+                                                     G_LOG_LEVEL_INFO,
+                                                     formatted_message->str);
+      g_string_assign (formatted_message, info_message);
+      g_free (info_message);
+    }
+  }
+
+  g_string_append_c (formatted_message, '\n');
+
+  for (i = 0; i < n_fields; i++) {
+    char* value = NULL;
+
+    if (fields[i].length == -1)
+      value = g_strescape ((const char*) fields[i].value, "\t'\"");
+
+    g_string_append_printf (formatted_message,
+                            "'%s': %s\n",
+                            fields[i].key,
+                            value != NULL ? value : "<binary data>");
+
+    if (value != NULL)
+      g_free (value);
+  }
+
+  gtu_log_diagnostic (formatted_message->str);
+  g_string_free (formatted_message, true);
+
+  if (level & G_LOG_FLAG_FATAL && !suppressed)
+    gtu_log_bail_out ("Fatal structured message received");
+
+ret:
+  return G_LOG_WRITER_HANDLED;
+}
+
+static gboolean fatal_handler (const char* domain,
+                               GLogLevelFlags level,
+                               const char* message,
+                               void* data)
+{
+  (void) level;
+  (void) message;
+  (void) data;
+
+  return domain == NULL ? false : g_str_has_prefix (domain, GTU_LOG_DOMAIN);
+}
+
+static void log_empty_plan (void) {
+  gtu_log_test_plan (0);
+}
+
+void gtu_log_g_install_handlers (void) {
+  static volatile size_t has_installed;
+
+  if (g_once_init_enter (&has_installed)) {
+    g_set_print_handler (&stdfd_handler);
+    g_set_printerr_handler (&stdfd_handler);
+
+    g_log_set_default_handler (&message_handler, NULL);
+    g_log_set_writer_func (&structured_handler, NULL, NULL);
+
+    g_test_log_set_fatal_handler (&fatal_handler, NULL);
+
+    atexit (&log_empty_plan);
+
+    g_once_init_leave (&has_installed, 1);
+  }
+}
+
+void gtu_log_g_install_suppress_func (GtuLogGSuppressFunc func,
+                                      const void* user_data)
+{
+  G_LOCK (suppress_info);
+
+  if (suppress_info.func || suppress_info.data) {
+    G_UNLOCK (suppress_info);
+    g_return_if_reached ();
+  }
+
+  suppress_info.func = func;
+  suppress_info.data = user_data;
+
+  G_UNLOCK (suppress_info);
+}
+
+void gtu_log_g_uninstall_suppress_func (GtuLogGSuppressFunc func) {
+  G_LOCK (suppress_info);
+
+  if (suppress_info.func != func) {
+    G_UNLOCK (suppress_info);
+    g_return_if_reached ();
+  }
+
+  memset (&suppress_info, '\0', sizeof (suppress_info));
+
+  G_UNLOCK (suppress_info);
+}

src/Makefile.am
@@ -6,12 +6,20 @@ else
 noinst_LIBRARIES = libgtu.a
 endif
 
+BUILT_SOURCES = log/color-table.c.in
+
+log/color-table.c.in:
+	$(AM_V_GEN) log/generate-color-table.py > $@
+
 libgtu_a_SOURCES = \
 	init.c \
-	log.c \
 	flags.c \
 	path.c \
 	object.c \
+	log/tap.c \
+	log/color.c \
+	log/glib-handlers.c \
+	log/glib-format.c \
 	test-case/test-case.c \
 	test-case/run.c \
 	test-suite/test-suite.c \

src/gtu-priv.h
@@ -52,18 +52,6 @@ G_GNUC_INTERNAL bool _gtu_test_case_has_run (GtuTestCase* self);
 /* returns array of GtuTestObject */
 G_GNUC_INTERNAL GPtrArray* _gtu_test_case_get_deps (GtuTestCase* self);
 
-/* Returns `true' if `self' handled the message, `false' otherwise. The
-   `caller' parameter should be the address of the function acting as a GLib
-   log handler. */
-G_GNUC_INTERNAL bool _gtu_test_case_handle_message (GtuTestCase* self,
-                                                    const char* domain,
-                                                    GLogLevelFlags level,
-                                                    const char* message,
-                                                    uintptr_t caller);
-
-/* The test currently being executed. May be NULL */
-G_GNUC_INTERNAL extern GtuTestCase* _gtu_current_test;
-
 typedef struct {
   GList* path_selectors;
   GList* path_skippers;

src/log.c
@@ -1,414 +1,0 @@
-#define _POSIX_C_SOURCE 201111L
-#include <stdio.h>
-#include <string.h>
-#include "log.h"
-
-/* TAP-compliant GLib message handling */
-
-static bool has_logged_plan = false;
-
-static bool should_log (GLogLevelFlags log_level) {
-  if (log_level & G_LOG_FLAG_FATAL)
-    return true;
-
-  switch (log_level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:
-    case G_LOG_LEVEL_CRITICAL:
-    case G_LOG_LEVEL_WARNING:
-      return true;
-
-    case G_LOG_LEVEL_MESSAGE:
-      return !(gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_QUIET);
-
-    case G_LOG_LEVEL_INFO:
-    case G_LOG_LEVEL_DEBUG:
-      return gtu_test_mode_flags_get_flags () & GTU_TEST_MODE_FLAGS_VERBOSE;
-
-    default:
-      g_assert_not_reached ();
-  }
-}
-
-/* we probably don't want to be calling isatty() all the time for something
-   trivial, so we wrap it in a once_init */
-static bool supports_color (void) {
-  /* -1 for unsupported, +1 for supported */
-  static volatile gssize supported = 0;
-
-  if (g_once_init_enter (&supported)) {
-    gssize result = g_log_writer_supports_color (fileno (stdout)) ? +1 : -1;
-    g_once_init_leave (&supported, result);
-  }
-
-  return supported > 0 ? true : false;
-}
-
-static const char* level_color (GLogLevelFlags level) {
-  if (!supports_color ())
-    return "";
-
-  if (level & G_LOG_FLAG_FATAL)
-    level = G_LOG_LEVEL_ERROR;
-
-  switch (level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:     return "\033[1;31m";
-    case G_LOG_LEVEL_CRITICAL:  return "\033[1;35m";
-    case G_LOG_LEVEL_WARNING:   return "\033[1;33m";
-    case G_LOG_LEVEL_MESSAGE:   return "\033[1;32m";
-    case G_LOG_LEVEL_INFO:      return "\033[1;34m";
-    case G_LOG_LEVEL_DEBUG:     return "\033[1;36m";
-  }
-
-  return "";
-}
-
-static const char* color_reset (void) {
-  return supports_color () ? "\033[0m" : "";
-}
-
-static const char* diagnostic (void) {
-  static char* string = NULL;
-
-  if (string == NULL)
-    string = g_strdup_printf ("%s#%s ",
-                              supports_color () ? "\033[1m" : "",
-                              color_reset ());
-
-  return string;
-}
-
-static const char* level_to_string (GLogLevelFlags log_level) {
-#define ret(str) {                    \
-  if (log_level & G_LOG_FLAG_FATAL) { \
-    return "FATAL-" str;              \
-  } else {                            \
-    return str;                       \
-  }                                   \
-}
-
-  switch (log_level & G_LOG_LEVEL_MASK) {
-    case G_LOG_LEVEL_ERROR:
-      ret ("ERROR");
-
-    case G_LOG_LEVEL_CRITICAL:
-      ret ("CRITICAL");
-
-    case G_LOG_LEVEL_WARNING:
-      ret ("WARNING");
-
-    case G_LOG_LEVEL_MESSAGE:
-      ret ("MESSAGE");
-
-    case G_LOG_LEVEL_INFO:
-      ret ("INFO");
-
-    case G_LOG_LEVEL_DEBUG:
-      ret ("DEBUG");
-
-    default:
-      g_assert_not_reached ();
-  }
-
-#undef ret
-}
-
-/* `message' may be NULL. If non-NULL, it should contain a trailing new line */
-static void bail_out (GLogLevelFlags level, const char* message) {
-  GtuDebugFlags debug_flags = _gtu_debug_flags_get ();
-
-  fprintf (stdout, "%sBail out!%s", level_color (level), color_reset ());
-  if (message != NULL)
-    fprintf (stdout, " %s", message);
-  else
-    fprintf (stdout, "\n");
-
-  if ((level & G_LOG_LEVEL_WARNING &&
-       debug_flags & GTU_DEBUG_FLAGS_FATAL_WARNINGS) ||
-      (level & G_LOG_LEVEL_CRITICAL &&
-       debug_flags & GTU_DEBUG_FLAGS_FATAL_CRITICALS))
-  {
-    g_abort ();
-  }
-
-  /* exit status to signal an error to the automake harness */
-  exit (99);
-}
-
-static void glib_test_logger (const char* log_domain, GLogLevelFlags log_level,
-                              const char* message, void* data)
-{
-  bool suppress = false;
-
-  (void) data;
-
-  if (_gtu_current_test != NULL &&
-      _gtu_test_case_handle_message (_gtu_current_test,
-                                     log_domain,
-                                     log_level,
-                                     message,
-                                     (uintptr_t) &glib_test_logger))
-  {
-    suppress = true;
-    if (should_log (G_LOG_LEVEL_INFO))
-      goto do_log;
-    else
-      return;
-  }
-
-  if (should_log (log_level)) {
-    char *fmtmsg;
-
-do_log:
-    fmtmsg = _gtu_log_format_message (log_domain, log_level, message);
-
-    if (suppress) {
-      char* smsg = g_strconcat ("Suppressed message: ", fmtmsg, NULL);
-      g_free (fmtmsg);
-      fmtmsg = _gtu_log_format_message (GTU_LOG_DOMAIN, G_LOG_LEVEL_INFO, smsg);
-      g_free (smsg);
-    }
-
-    if (log_level & G_LOG_FLAG_FATAL && !suppress) {
-      bail_out (log_level, fmtmsg);
-
-    } else {
-      _gtu_log_printf (fmtmsg);
-      g_free (fmtmsg);
-    }
-  }
-}
-
-static GLogWriterOutput glib_structured_logger (GLogLevelFlags log_level,
-                                                const GLogField* fields,
-                                                size_t n_fields,
-                                                void* data)
-{
-  size_t i;
-  bool suppress = false;
-
-  (void) data;
-
-  if (_gtu_current_test != NULL) {
-    const char* message = NULL;
-    const char* domain = NULL;
-
-    uintptr_t us = (uintptr_t) &glib_structured_logger;
-
-    for (i = 0; i < n_fields && (message == NULL || domain == NULL); i++) {
-      const char** var;
-
-      if (strcmp (fields[i].key, "MESSAGE") == 0) {
-        var = &message;
-      } else if (strcmp (fields[i].key, "GLIB_DOMAIN")) {
-        var = &domain;
-      } else {
-        continue;
-      }
-
-      g_return_val_if_fail (fields[i].length == -1, G_LOG_WRITER_UNHANDLED);
-      *var = (const char*) &fields[i].value;
-    }
-
-    if (message != NULL && _gtu_test_case_handle_message (_gtu_current_test,
-                                                          domain,
-                                                          log_level,
-                                                          message,
-                                                          us))
-    {
-      suppress = true;
-      if (should_log (G_LOG_LEVEL_INFO))
-        goto do_log;
-      else
-        goto ret;
-    }
-  }
-
-  if (!should_log (log_level))
-    goto ret;
-
-do_log:
-  _gtu_log_printf ("%s: new structured message:\n",
-                   level_to_string (log_level));
-
-  for (i = 0; i < n_fields; i++) {
-    char* value = NULL;
-
-    if (fields[i].length == -1)
-      value = g_strescape ((const char*) fields[i].value, "\t'\"");
-
-    _gtu_log_printf ("  '%s': %s\n",
-                     fields[i].key,
-                     value != NULL ? value : "<binary data>");
-
-    if (value != NULL)
-      g_free (value);
-  }
-
-  if (log_level & G_LOG_FLAG_FATAL && !suppress) {
-    bail_out (log_level, NULL);
-  }
-
-ret:
-  return G_LOG_WRITER_HANDLED;
-}
-
-static gboolean fatal_log_handler (const char* log_domain,
-                                   GLogLevelFlags log_level,
-                                   const char* message,
-                                   void* data)
-{
-  (void) log_level;
-  (void) message;
-  (void) data;
-
-  /* Fatal logs from us indicate a problem with the user's test suite.
-     Otherwise we never want GLib to abort for us, as we handle this
-     ourselves. */
-  return g_strcmp0 (log_domain, GTU_LOG_DOMAIN) == 0;
-}
-
-static void log_empty_test_plan (void) {
-  if (has_logged_plan || _gtu_get_test_mode ()->list_only)
-    return;
-
-  fprintf (stdout, "1..0 # Skipped: no tests to run\n");
-}
-
-static void glib_print_handler (const char* message) {
-  int i;
-  char buf;
-
-  /* To be TAP-compliant, lines written to stdout that aren't test results must
-     be prefixed with '#'. We iterate through `message', printing "# " after
-     every newline unless it's a trailing newline. */
-  for (i = 0, buf = '\n'; message[i] != '\0'; fputc (buf, stdout), i++) {
-    if (buf == '\n')
-      fprintf (stdout, diagnostic ());
-    buf = message[i];
-  }
-
-  if (buf != '\n')
-    fprintf (stdout, "\n");
-}
-
-static void glib_printerr_handler (const char* message) {
-  char* new_message = g_strconcat ("STDERR: ", message, NULL);
-  glib_print_handler (new_message);
-  g_free (new_message);
-}
-
-void _gtu_install_glib_loggers (void) {
-  g_set_print_handler (&glib_print_handler);
-  g_set_printerr_handler (&glib_printerr_handler);
-
-  g_log_set_default_handler (glib_test_logger, NULL);
-  g_log_set_writer_func (glib_structured_logger, NULL, NULL);
-
-  g_test_log_set_fatal_handler (&fatal_log_handler, NULL);
-
-  atexit (&log_empty_test_plan);
-}
-
-void _gtu_log_printf (const char* format, ...) {
-  va_list args;
-  char* message;
-
-  va_start (args, format);
-  message = g_strdup_vprintf (format, args);
-  glib_print_handler (message);
-  g_free (message);
-}
-
-void _gtu_log_test_plan (unsigned n_tests) {
-  g_assert (!has_logged_plan);
-
-  if (!_gtu_get_test_mode ()->list_only) {
-    if (n_tests == 0)
-      log_empty_test_plan ();
-    else
-      fprintf (stdout, "1..%u\n", n_tests);
-  }
-
-  has_logged_plan = true;
-}
-
-void _gtu_log_test_result (GtuTestResult result,
-                           const char* path,
-                           const char* message)
-{
-  static unsigned test_number = 1;
-
-  g_assert (path != NULL);
-
-  switch (result) {
-    case GTU_TEST_RESULT_PASS:
-    case GTU_TEST_RESULT_SKIP:
-      fprintf (stdout, "ok");
-      break;
-
-    case GTU_TEST_RESULT_FAIL:
-      fprintf (stdout, "not ok");
-      break;
-
-    default:
-      g_assert_not_reached ();
-  }
-
-  fprintf (stdout, " %d ", test_number++);
-  fprintf (stdout, path);
-
-  if (result == GTU_TEST_RESULT_PASS && message == NULL) {
-    fprintf (stdout, "\n");
-    return;
-  }
-
-  fprintf (stdout, " # ");
-
-  switch (result) {
-    case GTU_TEST_RESULT_PASS:
-      fprintf (stdout, "PASS");
-      break;
-
-    case GTU_TEST_RESULT_SKIP:
-      fprintf (stdout, "SKIP");
-      break;
-
-    case GTU_TEST_RESULT_FAIL:
-      fprintf (stdout, "FAIL");
-      break;
-
-    default:
-      g_assert_not_reached ();
-  }
-
-  if (message != NULL)
-    fprintf (stdout, " %s", message);
-
-  fprintf (stdout, "\n");
-}
-
-char* _gtu_log_format_message (const char* domain,
-                               GLogLevelFlags level,
-                               const char* message)
-{
-  const char *domain_head = "",
-             *domain_str  = "",
-             *domain_tail = "";
-
-  if (domain != NULL && domain[0] != '\0') {
-    domain_head = "(**";
-    domain_str = domain;
-    domain_tail = ") ";
-  }
-
-  return g_strdup_printf ("%s%s%s: %s%s%s%s",
-                          level_color (level),
-                          level_to_string (level),
-                          color_reset (),
-
-                          domain_head,
-                          domain_str,
-                          domain_tail,
-
-                          message);
-}

src/log.h
@@ -1,25 +1,0 @@
-#ifndef __GII_TEST_UTILS_TEST_LOG_PRIV_H__
-#define __GII_TEST_UTILS_TEST_LOG_PRIV_H__
-
-#include "gtu-priv.h"
-
-G_GNUC_INTERNAL void _gtu_install_glib_loggers (void);
-
-/* log raw message without any additional formatting beyond what's needed to
-   make it safe for the test harness */
-G_GNUC_INTERNAL void _gtu_log_printf (const char* format, ...);
-
-/* if `n_tests' is zero, logs an empty test plan marked 'skip' */
-G_GNUC_INTERNAL void _gtu_log_test_plan (unsigned n_tests);
-
-/* `message' may be NULL */
-G_GNUC_INTERNAL void _gtu_log_test_result (GtuTestResult result,
-                                           const char* path,
-                                           const char* message);
-
-/* No trailing newline. Free the result when done */
-G_GNUC_INTERNAL char* _gtu_log_format_message (const char* domain,
-                                               GLogLevelFlags level,
-                                               const char* message);
-
-#endif

commit f72d726c6e42250bb0c2ba5aa8c13afe31ab6d37

src: implement new log handling policy
This commit implements a new policy for handling messages logged through
GLib belonging to either the WARNING or CRITICAL category:
 - unexpected, non-GTU domains during test execution cause test abortion
 - GTU warnings/criticals at any time are elevated to FATAL
 - any warnings/criticals outside of test execution are elevated to
   FATAL
 - unexpected warnings/criticals during tests can be elevated to FATAL
   by setting the G_DEBUG environment variable appropriately

To aid in debugging, messages marked FATAL always produce a coredump.

This fixes a regression introduced by 6fba8ff7 which accidentally
dropped the code responsible for testing flags set in G_DEBUG.

src/init.c
@@ -132,8 +132,7 @@ static bool parse_args (char** args, int args_length) {
                GET_ARG ("--GTestSkipCount") ||
                strcmp  ("--GTestSubprocess", args[i]) == 0)
     {
-      g_log (GTU_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
-             "unsupported command line argument: %s", args[i]);
+      gtu_log_bail_out (false, "unsupported command line argument: %s", args[i]);
     }
   }
 
@@ -150,6 +149,13 @@ GtuLogAction verbosity_handler (const GtuLogGMessage* message, void* data) {
 
   if (message->flags & G_LOG_FLAG_FATAL)
     return GTU_LOG_ACTION_BAIL_OUT;
+
+  /* if the message is an unexpected warning or critical:
+   *   - if we're running a test, abort it
+   *   - otherwise, bail out of the test program */
+  if (message->flags & (G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL))
+    return _gtu_have_tr_context () ?
+      GTU_LOG_ACTION_ABORT : GTU_LOG_ACTION_BAIL_OUT;
 
   return _gtu_should_log (message->flags) ?
     GTU_LOG_ACTION_CONTINUE :
@@ -193,6 +199,24 @@ void gtu_init (char** args, int args_length) {
       _debug_keys,
       G_N_ELEMENTS (_debug_keys)
     );
+
+    /* g_test_init() sets the always-fatal mask to:
+     *   G_LOG_LEVEL_CRITICAL | G_LOG_LEVEL_WARNING
+     * It seems the intended policy is that all such messages result in a trap
+     * unless they're expected by a test case.
+     *
+     * We follow the same policy outside of the execution of test cases, but
+     * during the test runner we would prefer instead to fail the test and
+     * continue to the next. We also want this to be configurable via G_DEBUG.
+     * This is the intended logic:
+     *   - always-fatal mask is set according to G_DEBUG, per normal
+     *   - logged messages with the FATAL flag set always produce a trap, per
+     *     normal
+     *   - if no test is running or the domain belongs to GTU, criticals and
+     *     warnings are treated as though FATAL was set and produce a trap
+     *   - if a test is running and the domain does not belong to GTU, that
+     *     test is treated as though it had failed an assert (marked as failed
+     *     and stopped) */
 
     gtu_log_hooks_init (GTU_LOG_DOMAIN, &_gtu_test_preempt);
     gtu_log_hooks_push (&verbosity_handler, NULL);

src/log/glib-handlers.c
@@ -63,7 +63,7 @@ static void message_bailout (const char* domain,
                              const char* message)
 {
   char* formatted_message = gtu_log_g_format_message (domain, level, message);
-  gtu_log_bail_out (false, formatted_message);
+  gtu_log_bail_out (true, formatted_message);
 }
 
 static void message_handler (const char* domain,
@@ -74,10 +74,13 @@ static void message_handler (const char* domain,
   (void) data;
 
   if (!SHOULD_SUPPRESS (domain, level, message)) {
-    if (level & G_LOG_FLAG_FATAL)
+    if (level & G_LOG_FLAG_FATAL) {
+      /* in practice we shouldn't ever get here; this code is more illustrative
+       * than functional */
       message_bailout (domain, level, message);
-    else
+    } else {
       message_printer (domain, level, message);
+    }
   }
 }
 
@@ -148,8 +151,10 @@ static GLogWriterOutput structured_handler (GLogLevelFlags level,
   gtu_log_diagnostic (formatted_message->str);
   g_string_free (formatted_message, true);
 
+  /* in practice we shouldn't ever get here; this code is more illustrative
+   * than functional */
   if (level & G_LOG_FLAG_FATAL)
-    gtu_log_bail_out (false, "Fatal structured message received");
+    gtu_log_bail_out (true, "Fatal structured message received");
 
 ret:
   return G_LOG_WRITER_HANDLED;

src/log/hooks.c
@@ -164,9 +164,9 @@ static bool invoke_hooks (const GtuLogGMessage* message, void* user_data) {
       do_abort ();
 
     if (action == GTU_LOG_ACTION_BAIL_OUT)
-      gtu_log_bail_out (false, gtu_log_g_format_message (message->domain,
-                                                         message->flags,
-                                                         message->body));
+      gtu_log_bail_out (true, gtu_log_g_format_message (message->domain,
+                                                        message->flags,
+                                                        message->body));
 
     g_assert_not_reached ();
   }

src/log/log-hooks.h
@@ -44,8 +44,8 @@ typedef enum {
   /**
    * GTU_LOG_ACTION_BAIL_OUT:
    *
-   * Signals to the test harness that a fatal error has occured and exits the
-   * program.
+   * Signals to the test harness that a fatal error has occured and raises
+   * SIGTRAP.
    */
   GTU_LOG_ACTION_BAIL_OUT
 

src/test-case/priv-setjmp.h
@@ -13,6 +13,8 @@ typedef struct {
 
 G_GNUC_INTERNAL TestRunContext* _gtu_get_tr_context (void);
 
+G_GNUC_INTERNAL bool _gtu_have_tr_context (void);
+
 G_GNUC_INTERNAL GtuTestResult _gtu_test_case_exec_inner (GtuTestCaseFunc func,
                                                          void* func_target,
                                                          char** message);

src/test-case/run-init.c
@@ -14,11 +14,8 @@ static GtuLogAction log_hook (const GtuLogGMessage* message, void* user_data) {
 
   g_assert (message != NULL);
 
-  if (message->domain == NULL ||
-      g_str_has_prefix (message->domain, GTU_LOG_DOMAIN))
-  {
-    return GTU_LOG_ACTION_CONTINUE;
-  }
+  if (message->domain == NULL || message->flags & G_LOG_FLAG_FATAL)
+    return GTU_LOG_ACTION_CONTINUE; /* pass it on */
 
   priv = PRIVATE (self);
 

src/test-case/run-setjmp.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "log/logio.h"
 #include "test-case/priv-setjmp.h"
 
 /* lazy sanity checking */
@@ -24,6 +25,10 @@ static TestRunContext* _current_tr_context = NULL;
 
 TestRunContext* _gtu_get_tr_context (void) {
   return CURRENT_CONTEXT;
+}
+
+bool _gtu_have_tr_context (void) {
+  return _current_tr_context != NULL;
 }
 
 /* No asserts because anything log related will fail; we're in this function
@@ -52,8 +57,7 @@ void _gtu_assertion_message (const char* file,
     g_printerr ("**\nERROR:%s\n", location_message);
     abort ();
   } else if (!_gtu_keep_going) {
-    g_log (GTU_LOG_DOMAIN, G_LOG_LEVEL_MESSAGE | G_LOG_FLAG_FATAL,
-           location_message);
+    gtu_log_bail_out (false, location_message);
     g_assert_not_reached ();
   }
 